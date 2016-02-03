e48b17ddefd427818d932b45ea7379550289b6a3
Heather <campbell.hf@gmail.com>
2016-02-03 12:28:56 +0000

    We want to Start a fight by entering our names and seeing them

---
+++ battle_challenge/app.rb
@@ -0,0 +1,17 @@
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+  get '/' do
+    erb(:index)
+  end
+
+  post '/names' do
+    p params
+    @player1 = params[:player_1]
+    @player2 = params[:player_2]
+    erb(:play)
+
+  end
+      # # start the server if ruby file executed directly
+      run! if app_file == $0
+end
---
+++ battle_challenge/spec/features/name_spec.rb
@@ -0,0 +1,13 @@
+# require 'capybara'
+require 'spec_helper'
+# visit('http://localhost:4567/')
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1, with: 'Heather'
+    fill_in :player_2, with: 'Birthday'
+    click_button 'Submit'
+    expect(page).to have_content 'Heather vs. Birthday'
+  end
+end
